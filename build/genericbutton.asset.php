02cc67d222d2e58fce2a2c54c818ac83efd2e85d
Kaja Grabowska <kaja@wustl.edu>
2023-01-25 12:18:18 -0600

    Added color picker for the button block.

--- build/genericbutton.asset.php
+++ build/genericbutton.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '52945039e10c32def8d5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'ac09ffd752c699c8b81d');
