58c0a9f909c437b8b54f12ae9e3bd9c5ec58d683
Kaja Grabowska <kaja@wustl.edu>
2023-02-01 10:48:32 -0600

    Converted global header and footer to blocks.

--- functions.php
+++ functions.php
@@ -234,6 +234,7 @@ class PlaceholderBlock {
 
 new PlaceholderBlock('eventsandblogs');
 new PlaceholderBlock('header');
+new PlaceholderBlock('footer');
 
 class registerCustomBlock {
     function __construct($name, $renderCallback = null, $data = null) {
