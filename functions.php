40c9dbdb4d3e1f8f4a3d10efae2268a11f3cf756
Kaja Grabowska <kaja@wustl.edu>
2023-02-06 16:00:59 -0600

    Converted templates for single post and single page to blocks. Added blogindex placeholder block.

--- functions.php
+++ functions.php
@@ -235,6 +235,9 @@ class PlaceholderBlock {
 new PlaceholderBlock('eventsandblogs');
 new PlaceholderBlock('header');
 new PlaceholderBlock('footer');
+new PlaceholderBlock('singlepost');
+new PlaceholderBlock('page');
+new PlaceholderBlock('blogindex');
 
 class registerCustomBlock {
     function __construct($name, $renderCallback = null, $data = null) {
